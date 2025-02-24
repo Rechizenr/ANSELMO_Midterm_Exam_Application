7ae0d4c618f17f1c073e9eaf7c87b8e20c1f446f
Belmar Anselmo <echizenr96@gmail.com>
2025-02-24 20:53:26 +0800

    created controller and defined the products

---
+++ ANSELMO_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = [
+            ['name' => 'Naruto Poster', 'price' => 300, 'category' => 'Merchandise'],
+            ['name' => 'One Piece T-Shirt', 'price' => 800, 'category' => 'Clothing'],
+            ['name' => 'Attack on Titan Figuer', 'price' => 1500, 'category' => 'Action Figure'],
+            ['name' => 'Demon Slayer Sword', 'price' => 2500, 'category' => 'Collectibles'],
+            ['name' => 'Jujutsu Kaisen Keychain', 'price' => 300, 'category' => 'Accessories'],
+        ];
+
+        return view('products', compact('products'));
+    }
+}
